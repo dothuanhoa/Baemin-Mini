dd76bb8d805702c09574b6ce88fca633b0584bb7
dothuanhoa <119615081+dothuanhoa@users.noreply.github.com>
2026-07-12 14:45:28 +0700

    Init Project

---
+++ Baemin_Mini/src/main/java/com/baemin_mini/BaeminMiniApplication.java
@@ -0,0 +1,13 @@
+package com.baemin_mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeminMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeminMiniApplication.class, args);
+    }
+
+}
